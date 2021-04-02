9ffe2025a2a3cf1639066d376c7bf59ea99f078c
hugsbord <s72h@Macbook-Pro.local>
2021-04-02 13:00:06 +0200

    remove ft_itoa from libft

--- srcs/cub3d_utils.c
+++ srcs/cub3d_utils.c
@@ -6,39 +6,11 @@
 /*   By: hugsbord <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 09:49:03 by hugsbord          #+#    #+#             */
-/*   Updated: 2021/04/02 12:37:23 by hugsbord         ###   ########.fr       */
+/*   Updated: 2021/04/02 12:58:39 by hugsbord         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/cub3d.h"
-
-/*int			ft_atoi(const char *str)
-{
-	int		i;
-	int		sign;
-	int		res;
-
-	i = 0;
-	res = 0;
-	sign = 1;
-	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (ft_isdigit(str[i]))
-	{
-		res = res * 10 + str[i] - 48;
-		i++;
-	}
-	res *= sign;
-	return (res);
-}*/
-
-
 
 int			ft_skip_spaces2(t_data  *data, int x, int y)
 {
